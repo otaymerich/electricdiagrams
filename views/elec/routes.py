d46c7e0ad71a3066634dc55dcbd9208e20abb584
otaymerich <otaymerich@gmail.com>
2022-09-22 12:27:29 +0200

    Updates elec/routes.py
    Created route / to template base.html

--- views/elec/routes.py
+++ views/elec/routes.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template, request, session, url_for, redirect
 
 elec = Blueprint("elec", __name__)
+
+@elec.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("base.html")
