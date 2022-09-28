de4875170bb0e799060071ef4f2f13adeea8b59e
otaymerich <otaymerich@gmail.com>
2022-09-28 11:38:53 +0200

    Update models.py
    Update class House

--- views/elec/models.py
+++ views/elec/models.py
@@ -46,12 +46,15 @@ class House(db.Model):
     domotics = db.Column(db.Boolean)
     elec_car = db.Column(db.Boolean)
     solar_panels = db.Column(db.Boolean)
+    climate_outdoor_unit = db.Column(db.Interger, nullable=True)
+    climate_indoor_unit = db.Column(db.Interger, nullable=True)
+
 
     @staticmethod
     def gen_id():
         return uuid4().hex
 
-    def __init__(self, user_id: str, proj_title: str, address: str, m2: float, floors: int, pool: bool, garden: bool, fridge: bool, freezer: bool, oven: bool, vitro_hub: bool, dishwasher: bool, boiler: bool, aerothermia: bool, electric_heater: bool, wash_machine: bool, dryer: bool, iron: bool, alarm: bool, electronics: bool, domotics: bool, elec_car: bool, solar_panels: bool):
+    def __init__(self, user_id: str, proj_title: str, address: str, m2: float, floors: int, pool: bool, garden: bool, fridge: bool, freezer: bool, oven: bool, vitro_hub: bool, dishwasher: bool, boiler: bool, aerothermia: bool, electric_heater: bool, wash_machine: bool, dryer: bool, iron: bool, alarm: bool, electronics: bool, domotics: bool, elec_car: bool, solar_panels: bool, climate_outdoor_unit=None, climate_indoor_unit=None):
         self.id = self.gen_id()
         self.user_id = user_id
         self.proj_title = proj_title
@@ -76,6 +79,6 @@ class House(db.Model):
         self.domotics = domotics
         self.elec_car = elec_car
         self.solar_panels = solar_panels
-
-
+        self.climate_outdoor_unit = climate_outdoor_unit
+        self.climate_indoor_unit = climate_indoor_unit
 
