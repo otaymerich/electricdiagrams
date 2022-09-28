ae701da899bae0ffd17f2750cd73ffc9a7c49649
otaymerich <otaymerich@gmail.com>
2022-09-28 09:02:40 +0200

    Update base.html elec/routes.py

--- views/elec/routes.py
+++ views/elec/routes.py
@@ -1,26 +1,36 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from auth_obj import Auth
 from views.elec.models import db, Users
 
 elec = Blueprint("elec", __name__)
 test_auth = Auth(session, Users, "elec.t_login", "elec.home", request, db)
 
+'''
+HOME
+'''
 @elec.route("/", methods=["GET", "POST"])
 @test_auth.auth
 def home():
     return render_template("base.html")
 
+'''
+LOGIN/LOGOUT
+'''
 @elec.route("/login", methods=["GET", "POST"])
 @test_auth.login_check
 def t_login():
     return render_template("login.html")
 
+@elec.route("/logout")
+def log_out():
+    session.clear()
+    return redirect(url_for("elec.t_login"))
+
+
 
 @elec.route("/test")
 def t_test():
-    user = Users.add_user("test1", "test1@email.com", "1234")
-    db.session.add(user)
-    db.session.commit()
+    # user = Users.add_user("test1", "test1@email.com", "1234")
+    # db.session.add(user)
+    # db.session.commit()
     return "test"
-if __name__ == "__main__":
-    print(type(request))
